2798fcaa6890400e7b0984e76ebf6fb525d8c5b2
manjeetsinghmeena <manjeetsinghmeena6@gmail.com>
2026-03-13 19:35:21 +0530

    first commit

---
+++ my/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import "./Navbar.css"
+function Navbar(){
+    return (
+        <div className="navbar">
+            <ul>
+                <li><a>Home</a></li>
+                <li><a>Contact</a></li>
+                <li><a>About Us</a></li>
+            </ul>
+        </div>
+    )
+}
+export default Navbar
---
+++ my/src/components/hero.jsx
@@ -0,0 +1,12 @@
+import "./hero.css"
+function Hero(){
+    return (
+        <section className="hero">
+            <div className="heroText">
+                <h1>Bar Palladio</h1>
+                <p>Every cup is a masterpiece, crafted with passion and precision.</p>
+            </div>
+        </section>
+    )
+}
+export default Hero
