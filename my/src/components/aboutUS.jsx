78bd547074c51b0d3d3940dc04340826f8646aba
manjeetsinghmeena <manjeetsinghmeena6@gmail.com>
2026-03-17 22:30:03 +0530

    eigth commit

--- my/src/components/aboutUS.jsx
+++ my/src/components/aboutUS.jsx
@@ -1,15 +1,18 @@
+import "./aboutUs.css";
 function AboutUs() {
     return (
         <div className="about">
+            <div className="content">
             <div className="photo">
-                <img src="/bar palladio.jpg" alt="Bar Palladio" />
+                <img src="/bar_palladio.jpg" alt="Bar Palladio" />
             </div>
             <div className="description">
                 <h4>THE INTERIOR</h4>
                 <h1 className="pi">Painted in</h1>
                 <h1 className="ii">indigo and ivory</h1>
-                <p>Every surface of Bar Palladio is a canvas — hand-painted Mughal floral inlay on deep cobalt blue, arched colonnades, marble floors, and silk-embroidered cushions that belong to another century entirely.</p>
+                <p className="para2">Every surface of Bar Palladio is a canvas — hand-painted Mughal floral inlay on deep cobalt blue, arched colonnades, marble floors, and silk-embroidered cushions that belong to another century entirely.</p>
             </div>
+        </div>
         </div>
     );
 }
