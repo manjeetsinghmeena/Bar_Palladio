eac77faa76d9f81896affea1c368d15ab0376dfc
manjeetsinghmeena <manjeetsinghmeena6@gmail.com>
2026-03-21 22:06:26 +0530

    eleventh commit

--- my/src/components/hero.jsx
+++ my/src/components/hero.jsx
@@ -6,12 +6,16 @@ function Hero(){
         <div className="quote">
             <div className=" para"><p>"One of the world's most beautiful restaurants — a Mughal jewel box that happens to serve the finest Negroni east of Milan."</p></div>
             <p >— Condé Nast Traveller</p>
-            </div>  
+            </div> 
+            {/* <div className="stack">
+            <img src="/left.png" alt="logo" className="left"/> */}
             <div className="title">
                 <h2 className="B">Bar</h2>
                 <h1 className="P">Palladio</h1>
+                {/* <img src="/right.png" alt="logo" className="right"/> */}
+                </div>
                 <h3 className="J">Jaipur · Est. 2016</h3>
-            </div>
+            {/* </div> */}
             <div className="divider"></div>
             <button className="btn">RESERVE TABLE</button>
         </section>
