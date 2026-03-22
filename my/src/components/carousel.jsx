93255621bd1626023a22b94c7b183d91f8cd35fb
manjeetsinghmeena <manjeetsinghmeena6@gmail.com>
2026-03-22 21:36:21 +0530

    thirteenth commit

--- my/src/components/carousel.jsx
+++ my/src/components/carousel.jsx
@@ -1,14 +1,15 @@
+import "./carousel.css";
 function Carousel() {
     return (
         <div className="carousel">
-            <div className="menu">FROM THE KITCHEN</div>
+            <div className="menu-text">FROM THE KITCHEN</div>
             <div className="carousel-content">
                 <div className="carousel-item">
-                    <img src="/menu1.jpg" alt="Food 1" className="menu1" />
-                    <img src="/menu2.jpg" alt="Food 2" className="menu2" />
-                    <img src="/menu3.jpg" alt="Food 3" className="menu3" />     
-                    <img src="/menu4.jpg" alt="Food 4" className="menu4" />
-                    <img src="/menu5.jpg" alt="Food 5" className="menu5" />
+                    <img src="/menu1.jpg" alt="Food 1" className="menu" />
+                    <img src="/menu2.jpg" alt="Food 2" className="menu" />
+                    <img src="/menu3.jpg" alt="Food 3" className="menu" />     
+                    <img src="/menu4.jpg" alt="Food 4" className="menu" />
+                    <img src="/menu5.jpg" alt="Food 5" className="menu" />
                 </div>
             </div>
         </div>
