4224c41960f65670d9862ad89d28a0f5d913376a
manjeetsinghmeena <manjeetsinghmeena6@gmail.com>
2026-03-23 14:52:00 +0530

    fifteenth commit

--- my/src/components/carousel.jsx
+++ my/src/components/carousel.jsx
@@ -10,6 +10,61 @@ function Carousel() {
                     <img src="/menu3.jpg" alt="Food 3" className="menu" />     
                     <img src="/menu4.jpg" alt="Food 4" className="menu" />
                     <img src="/menu5.jpg" alt="Food 5" className="menu" />
+                     <img src="/menu1.jpg" alt="Food 1" className="menu" />
+                    <img src="/menu2.jpg" alt="Food 2" className="menu" />
+                    <img src="/menu3.jpg" alt="Food 3" className="menu" />     
+                    <img src="/menu4.jpg" alt="Food 4" className="menu" />
+                    <img src="/menu5.jpg" alt="Food 5" className="menu" />
+                     <img src="/menu1.jpg" alt="Food 1" className="menu" />
+                    <img src="/menu2.jpg" alt="Food 2" className="menu" />
+                    <img src="/menu3.jpg" alt="Food 3" className="menu" />     
+                    <img src="/menu4.jpg" alt="Food 4" className="menu" />
+                    <img src="/menu5.jpg" alt="Food 5" className="menu" />
+                     <img src="/menu1.jpg" alt="Food 1" className="menu" />
+                    <img src="/menu2.jpg" alt="Food 2" className="menu" />
+                    <img src="/menu3.jpg" alt="Food 3" className="menu" />     
+                    <img src="/menu4.jpg" alt="Food 4" className="menu" />
+                    <img src="/menu5.jpg" alt="Food 5" className="menu" />
+                     <img src="/menu1.jpg" alt="Food 1" className="menu" />
+                    <img src="/menu2.jpg" alt="Food 2" className="menu" />
+                    <img src="/menu3.jpg" alt="Food 3" className="menu" />     
+                    <img src="/menu4.jpg" alt="Food 4" className="menu" />
+                    <img src="/menu5.jpg" alt="Food 5" className="menu" />
+                     <img src="/menu1.jpg" alt="Food 1" className="menu" />
+                    <img src="/menu2.jpg" alt="Food 2" className="menu" />
+                    <img src="/menu3.jpg" alt="Food 3" className="menu" />     
+                    <img src="/menu4.jpg" alt="Food 4" className="menu" />
+                    <img src="/menu5.jpg" alt="Food 5" className="menu" />
+                     <img src="/menu1.jpg" alt="Food 1" className="menu" />
+                    <img src="/menu2.jpg" alt="Food 2" className="menu" />
+                    <img src="/menu3.jpg" alt="Food 3" className="menu" />     
+                    <img src="/menu4.jpg" alt="Food 4" className="menu" />
+                    <img src="/menu5.jpg" alt="Food 5" className="menu" />
+                     <img src="/menu1.jpg" alt="Food 1" className="menu" />
+                    <img src="/menu2.jpg" alt="Food 2" className="menu" />
+                    <img src="/menu3.jpg" alt="Food 3" className="menu" />     
+                    <img src="/menu4.jpg" alt="Food 4" className="menu" />
+                    <img src="/menu5.jpg" alt="Food 5" className="menu" />
+                     <img src="/menu1.jpg" alt="Food 1" className="menu" />
+                    <img src="/menu2.jpg" alt="Food 2" className="menu" />
+                    <img src="/menu3.jpg" alt="Food 3" className="menu" />     
+                    <img src="/menu4.jpg" alt="Food 4" className="menu" />
+                    <img src="/menu5.jpg" alt="Food 5" className="menu" />
+                     <img src="/menu1.jpg" alt="Food 1" className="menu" />
+                    <img src="/menu2.jpg" alt="Food 2" className="menu" />
+                    <img src="/menu3.jpg" alt="Food 3" className="menu" />     
+                    <img src="/menu4.jpg" alt="Food 4" className="menu" />
+                    <img src="/menu5.jpg" alt="Food 5" className="menu" />
+                     <img src="/menu1.jpg" alt="Food 1" className="menu" />
+                    <img src="/menu2.jpg" alt="Food 2" className="menu" />
+                    <img src="/menu3.jpg" alt="Food 3" className="menu" />     
+                    <img src="/menu4.jpg" alt="Food 4" className="menu" />
+                    <img src="/menu5.jpg" alt="Food 5" className="menu" />
+                     <img src="/menu1.jpg" alt="Food 1" className="menu" />
+                    <img src="/menu2.jpg" alt="Food 2" className="menu" />
+                    <img src="/menu3.jpg" alt="Food 3" className="menu" />     
+                    <img src="/menu4.jpg" alt="Food 4" className="menu" />
+                    <img src="/menu5.jpg" alt="Food 5" className="menu" />
                 </div>
             </div>
         </div>
